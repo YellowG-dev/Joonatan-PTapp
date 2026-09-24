67d7764b4e1acd2a52ec56b500e1822a257ee8fc
Juha <elojuha@gmail.com>
2026-09-24 22:14:05 +0300

    Wearables sync for coach app

--- src/core/program-joonatan.js
+++ src/core/program-joonatan.js
@@ -14,7 +14,7 @@
 
 export const PROGRAM_ID = "joonatan";
 export const CLIENT_NAME = "Joonatan";
-export const APP_VERSION = "5.0.0-beta1";
+export const APP_VERSION = "5.1.0-beta1";
 
 export const SLOTS = ["strength"];
 
